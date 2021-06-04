1ec7c66583d43bb5f2de3a44aecbce0efc8b8887
zxh0 <zhangxiuhong2013@gmail.com>
2021-06-04 16:50:11 +0800

    Remove spaces

--- tests/SmartBitcoinCash/TWCoinTypeTests.cpp
+++ tests/SmartBitcoinCash/TWCoinTypeTests.cpp
@@ -13,19 +13,19 @@
 #include <gtest/gtest.h>
 
 
-TEST(TWSmartBitcoin CashCoinType, TWCoinType) {
-    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeSmartBitcoin Cash));
+TEST(TWSmartBitcoinCashCoinType, TWCoinType) {
+    auto symbol = WRAPS(TWCoinTypeConfigurationGetSymbol(TWCoinTypeSmartBitcoinCash));
     auto txId = WRAPS(TWStringCreateWithUTF8Bytes("0xf455b0c26c1ebaedf80736cc3baab0d49093185cd8e0dd93b566270a36eb228b"));
-    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeSmartBitcoin Cash, txId.get()));
+    auto txUrl = WRAPS(TWCoinTypeConfigurationGetTransactionURL(TWCoinTypeSmartBitcoinCash, txId.get()));
     auto accId = WRAPS(TWStringCreateWithUTF8Bytes("0xffce56464b76bea1ef202e99bf56e7bf9ca837db"));
-    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeSmartBitcoin Cash, accId.get()));
-    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeSmartBitcoin Cash));
-    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeSmartBitcoin Cash));
+    auto accUrl = WRAPS(TWCoinTypeConfigurationGetAccountURL(TWCoinTypeSmartBitcoinCash, accId.get()));
+    auto id = WRAPS(TWCoinTypeConfigurationGetID(TWCoinTypeSmartBitcoinCash));
+    auto name = WRAPS(TWCoinTypeConfigurationGetName(TWCoinTypeSmartBitcoinCash));
 
-    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeSmartBitcoin Cash), 18);
-    ASSERT_EQ(TWBlockchainEthereum, TWCoinTypeBlockchain(TWCoinTypeSmartBitcoin Cash));
-    ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeSmartBitcoin Cash));
-    ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeSmartBitcoin Cash));
+    ASSERT_EQ(TWCoinTypeConfigurationGetDecimals(TWCoinTypeSmartBitcoinCash), 18);
+    ASSERT_EQ(TWBlockchainEthereum, TWCoinTypeBlockchain(TWCoinTypeSmartBitcoinCash));
+    ASSERT_EQ(0x0, TWCoinTypeP2shPrefix(TWCoinTypeSmartBitcoinCash));
+    ASSERT_EQ(0x0, TWCoinTypeStaticPrefix(TWCoinTypeSmartBitcoinCash));
     assertStringsEqual(symbol, "BCH");
     assertStringsEqual(txUrl, "https://smartbch-explorer.web.app//tx/0xf455b0c26c1ebaedf80736cc3baab0d49093185cd8e0dd93b566270a36eb228b");
     assertStringsEqual(accUrl, "https://smartbch-explorer.web.app//address/0xffce56464b76bea1ef202e99bf56e7bf9ca837db");
